95bf70dd87451ec4173cc31944836cb367bd6e36
SoMuchToGrok <zidvbelju@gmail.com>
2017-02-03 13:41:47 -0500

    hashes

--- functions.sh
+++ functions.sh
@@ -9,7 +9,7 @@ function die() {
 
 function bootstrap() {
     # We want to lock the states and pillar to a specific hash.
-    states_hash=94ce6eb4035b1ff7c7043aa9668803936d32343d
+    states_hash=bb798889721aa207b4b65c437c9623cffafd8a99
     pillar_hash=84a53b1462fc7e8a52fb7b3c9350a78e64fcfbcc
 
     if [[ -z $states_hash ]] || [[ -z $pillar_hash ]]; then
